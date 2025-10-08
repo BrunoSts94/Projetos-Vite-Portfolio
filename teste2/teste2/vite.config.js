2a34f3f63c2d0df8ed76241dd919f32ee4d5fc2f
BrunoSts94 <bruno_ps94@hotmail.com>
2025-10-08 11:15:04 -0300

    ajuste de url

--- teste2/teste2/vite.config.js
+++ teste2/teste2/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Projetos-Vite-Portfolio/teste2/', // 👈 caminho relativo no GitHub Pages
+  base: '/Projetos-Vite-Portfolio/teste2/teste2/', // 👈 caminho relativo no GitHub Pages
 })
